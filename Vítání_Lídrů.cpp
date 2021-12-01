f79a03c102dbde11861ffa13b4fa54207868099c
NetysGamesCZ <netysgamesyt@gmail.com>
2021-12-01 16:18:13 +0100

    C++ (MattyPew_)

---
+++ Vítání_Lídrů.cpp
@@ -0,0 +1,15 @@
+//#include <stdio.io>
+#include <iostream>
+using namespace std;
+
+void pozdrav(string jmeno)
+{
+    cout << "Ahoj, vrele te tu vitam " << jmeno << "!" << endl; 
+}
+
+int main()
+{
+pozdrav("Honzo");
+pozdrav("Juliane");
+
+}
